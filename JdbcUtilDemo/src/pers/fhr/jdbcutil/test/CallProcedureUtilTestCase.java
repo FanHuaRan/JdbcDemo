0a99ad8163f09a33d46d1b17bb80dda33a7bad36
FanHuaRan <834171100@qq.com>
2017-06-22 10:00:24 +0800

    2017/06/22 fhr 完善

---
+++ JdbcUtilDemo/src/pers/fhr/jdbcutil/test/CallProcedureUtilTestCase.java
@@ -0,0 +1,14 @@
+package pers.fhr.jdbcutil.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CallProcedureUtilTestCase {
+
+	@Test
+	public void testCallProcedureQuery() {
+		fail("Not yet implemented");
+	}
+
+}
